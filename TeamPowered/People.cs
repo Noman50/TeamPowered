127e921523fad0c9b0a7f2d1768f693bfda4b68e
Md Noman <Md Noman@DESTOP-NOMAN>
2017-04-13 11:48:55 +0800

    Split Name into First and last name

--- TeamPowered/People.cs
+++ TeamPowered/People.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class People
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
